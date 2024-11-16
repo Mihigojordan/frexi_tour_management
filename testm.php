e1552dc05db19079fa2760099d3cb028a944834a
12th-ai <mihigofololive@gmail.com>
2024-11-16 15:18:57 +0200

    update : update reviews

--- testm.php
+++ testm.php
@@ -13,19 +13,17 @@
               <div class="testi-card">
                 <div class="testi-card_wrapper">
                   <div class="testi-card_profile">
-                    <div class="testi-card_avater"><img src="assets/img/testimonial/testi_1_1.jpg" alt="testimonial">
+                    <div class="testi-card_avater"><img src="assets/img/testimonial/kabare.png" alt="testimonial">
                     </div>
                     <div class="media-body">
-                      <h3 class="box-title">Maria Doe</h3><span class="testi-card_desig">Traveller</span>
+                      <h3 class="box-title">Kabare Assoumah Mullay</h3><span class="testi-card_desig">Tour</span>
                     </div>
                   </div>
                   <div class="testi-card_review"><i class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i> <i
                       class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i>
                   </div>
                 </div>
-                <p class="testi-card_text">“A home that perfectly blends sustainability with luxury until I discovered
-                  Ecoland Residence. From the moment I stepped into this community, I knew it was where I wanted to
-                  live. The commitment to eco-friendly living”</p>
+                <p class="testi-card_text">“I had an unbelievable journey with amazing people and services too”</p>
                 <div class="testi-card-quote"><img src="assets/img/icon/testi-quote.svg" alt="img"></div>
               </div>
             </div>
@@ -33,19 +31,19 @@
               <div class="testi-card">
                 <div class="testi-card_wrapper">
                   <div class="testi-card_profile">
-                    <div class="testi-card_avater"><img src="assets/img/testimonial/testi_1_2.jpg" alt="testimonial">
+                    <div class="testi-card_avater"><img src="assets/img/testimonial/theo.png" alt="testimonial">
                     </div>
                     <div class="media-body">
-                      <h3 class="box-title">Andrew Simon</h3><span class="testi-card_desig">Traveller</span>
+                      <h3 class="box-title">Mugabarigira Theogene</h3><span class="testi-card_desig">Traveller</span>
                     </div>
                   </div>
                   <div class="testi-card_review"><i class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i> <i
                       class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i>
                   </div>
                 </div>
-                <p class="testi-card_text">“The home boasts sleek, contemporary architecture with clean lines and
-                  expansive windows, allowing natural light to flood the interiors It incorporates passive design
-                  principles”</p>
+                <p class="testi-card_text">“Thanks to FREXI services, my daughter moved safely from Dar to KGL via NBO!
+
+Thanks a lot”</p>
                 <div class="testi-card-quote"><img src="assets/img/icon/testi-quote.svg" alt="img"></div>
               </div>
             </div>
@@ -53,18 +51,58 @@
               <div class="testi-card">
                 <div class="testi-card_wrapper">
                   <div class="testi-card_profile">
-                    <div class="testi-card_avater"><img src="assets/img/testimonial/testi_1_1.jpg" alt="testimonial">
+                    <div class="testi-card_avater"><img src="assets/img/testimonial/mutesi.png" alt="testimonial">
                     </div>
                     <div class="media-body">
-                      <h3 class="box-title">Alex Jordan</h3><span class="testi-card_desig">Traveller</span>
+                      <h3 class="box-title">Vanessa Umutesi</h3><span class="testi-card_desig">Traveller</span>
                     </div>
                   </div>
                   <div class="testi-card_review"><i class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i> <i
                       class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i>
                   </div>
                 </div>
-                <p class="testi-card_text">“Solar panels adorn the roof, harnessing renewable energy to power the home
-                  and even feed excess electricity back into the grid. High-performance insulation and triple-glazed”
+                <p class="testi-card_text">“Frexi planned my amazing Zanzibar trip. They were super helpful, creating a perfect itinerary based on my wishes. The accommodations, transportation, and guides were all fantastic. I highly recommend Frexi for anyone visiting Zanzibar.”
+                </p>
+                <div class="testi-card-quote"><img src="assets/img/icon/testi-quote.svg" alt="img"></div>
+              </div>
+            </div>
+
+
+            <div class="swiper-slide">
+              <div class="testi-card">
+                <div class="testi-card_wrapper">
+                  <div class="testi-card_profile">
+                    <div class="testi-card_avater"><img src="assets/img/testimonial/uwera.png" alt="testimonial">
+                    </div>
+                    <div class="media-body">
+                      <h3 class="box-title">Uwera Ritah</h3><span class="testi-card_desig">Traveller</span>
+                    </div>
+                  </div>
+                  <div class="testi-card_review"><i class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i> <i
+                      class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i>
+                  </div>
+                </div>
+                <p class="testi-card_text">“Flexi Travels and Tours is amazing! Their team is so friendly and helpful, and they really take care of every little detail to make your trip special. They offer a variety of packages to choose from, and their professionalism makes planning a breeze. I highly recommend them for a stress-free and memorable travel experience!”</p>
+                <div class="testi-card-quote"><img src="assets/img/icon/testi-quote.svg" alt="img"></div>
+              </div>
+            </div>
+            <div class="swiper-slide">
+              <div class="testi-card">
+                <div class="testi-card_wrapper">
+                  <div class="testi-card_profile">
+                    <div class="testi-card_avater"><img src="assets/img/testimonial/cyiza.png" alt="testimonial">
+                    </div>
+                    <div class="media-body">
+                      <h3 class="box-title">Sixbert Cuzio</h3><span class="testi-card_desig">Traveller</span>
+                    </div>
+                  </div>
+                  <div class="testi-card_review"><i class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i> <i
+                      class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i>
+                  </div>
+                </div>
+                <p class="testi-card_text">
+                “I am very grateful for Frexi, they helped me got my first Schengen visa and best plane ticket. Not only that they made a follow up to be sure that I took the plane on time and reached my destination well.
+                ”
                 </p>
                 <div class="testi-card-quote"><img src="assets/img/icon/testi-quote.svg" alt="img"></div>
               </div>
@@ -73,18 +111,85 @@
               <div class="testi-card">
                 <div class="testi-card_wrapper">
                   <div class="testi-card_profile">
-                    <div class="testi-card_avater"><img src="assets/img/testimonial/testi_1_2.jpg" alt="testimonial">
+                    <div class="testi-card_avater"><img src="assets/img/testimonial/loic.png" alt="testimonial">
                     </div>
                     <div class="media-body">
-                      <h3 class="box-title">Maria Doe</h3><span class="testi-card_desig">Traveller</span>
+                      <h3 class="box-title">Loïc Kevin Rutayisire</h3><span class="testi-card_desig">Traveller</span>
                     </div>
                   </div>
                   <div class="testi-card_review"><i class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i> <i
                       class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i>
                   </div>
                 </div>
-                <p class="testi-card_text">A sophisticated rainwater harvesting system collects and filters rainwater
-                  for irrigation and non-potable uses, reducing reliance on municipal water sources. Greywater systems
+                <p class="testi-card_text">“The service is impeccable and on time, consistent assistance for any inquiry you have. It has been nothing but best quality service received.”</p>
+                <div class="testi-card-quote"><img src="assets/img/icon/testi-quote.svg" alt="img"></div>
+              </div>
+            </div>
+
+
+            
+            <div class="swiper-slide">
+              <div class="testi-card">
+                <div class="testi-card_wrapper">
+                  <div class="testi-card_profile">
+                    <div class="testi-card_avater"><img src="assets/img/testimonial/jack.png" alt="testimonial">
+                    </div>
+                    <div class="media-body">
+                      <h3 class="box-title">Jacques Nyilinkindi</h3><span class="testi-card_desig">Tour</span>
+                    </div>
+                  </div>
+                  <div class="testi-card_review"><i class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i> <i
+                      class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i>
+                  </div>
+                </div>
+                
+                <p class="testi-card_text">“Timely response and Good service”</p>
+                <div class="testi-card-quote"><img src="assets/img/icon/testi-quote.svg" alt="img"></div>
+              </div>
+            </div>
+
+            <div class="swiper-slide">
+              <div class="testi-card">
+                <div class="testi-card_wrapper">
+                  <div class="testi-card_profile">
+                    <div class="testi-card_avater"><img src="assets/img/testimonial/gabriel.png" alt="testimonial">
+                    </div>
+                    <div class="media-body">
+                      <h3 class="box-title">Gabriel Sheja</h3><span class="testi-card_desig">Traveller</span>
+                    </div>
+                  </div>
+                  <div class="testi-card_review"><i class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i> <i
+                      class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i>
+                  </div>
+                </div>
+                
+                <p class="testi-card_text">“Mon expérience avec Frexi été plus que Formidable,
+Le service bien servi
+Servi avec le sourire  Chaleureusement accueilli j'ai été.
+
+Je recommande à tous et toutes personnes.
+Merci Frexi.”</p>
+                <div class="testi-card-quote"><img src="assets/img/icon/testi-quote.svg" alt="img"></div>
+              </div>
+            </div>
+
+
+
+            <div class="swiper-slide">
+              <div class="testi-card">
+                <div class="testi-card_wrapper">
+                  <div class="testi-card_profile">
+                    <div class="testi-card_avater"><img src="assets/img/testimonial/eric.png" alt="testimonial">
+                    </div>
+                    <div class="media-body">
+                      <h3 class="box-title"> Emad Alamleh</h3><span class="testi-card_desig">Traveller</span>
+                    </div>
+                  </div>
+                  <div class="testi-card_review"><i class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i> <i
+                      class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i>
+                  </div>
+                </div>
+                <p class="testi-card_text">Professional, Trust Worthy, punctual, and great customer care....
                 </p>
                 <div class="testi-card-quote"><img src="assets/img/icon/testi-quote.svg" alt="img"></div>
               </div>
@@ -93,77 +198,17 @@
               <div class="testi-card">
                 <div class="testi-card_wrapper">
                   <div class="testi-card_profile">
-                    <div class="testi-card_avater"><img src="assets/img/testimonial/testi_1_1.jpg" alt="testimonial">
+                    <div class="testi-card_avater"><img src="assets/img/testimonial/akariza.png" alt="testimonial">
                     </div>
                     <div class="media-body">
-                      <h3 class="box-title">Angelina Rose</h3><span class="testi-card_desig">Traveller</span>
+                      <h3 class="box-title">KARIZA SAFARI Christella</h3><span class="testi-card_desig">Traveller</span>
                     </div>
                   </div>
                   <div class="testi-card_review"><i class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i> <i
                       class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i>
                   </div>
                 </div>
-                <p class="testi-card_text">Throughout the interior, eco-friendly materials like reclaimed wood, bamboo
-                  flooring, and recycled glass countertops create a luxurious yet sustainable ambiance.</p>
-                <div class="testi-card-quote"><img src="assets/img/icon/testi-quote.svg" alt="img"></div>
-              </div>
-            </div>
-            <div class="swiper-slide">
-              <div class="testi-card">
-                <div class="testi-card_wrapper">
-                  <div class="testi-card_profile">
-                    <div class="testi-card_avater"><img src="assets/img/testimonial/testi_1_1.jpg" alt="testimonial">
-                    </div>
-                    <div class="media-body">
-                      <h3 class="box-title">Maria Doe</h3><span class="testi-card_desig">Traveller</span>
-                    </div>
-                  </div>
-                  <div class="testi-card_review"><i class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i> <i
-                      class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i>
-                  </div>
-                </div>
-                <p class="testi-card_text">“A home that perfectly blends sustainability with luxury until I discovered
-                  Ecoland Residence. From the moment I stepped into this community, I knew it was where I wanted to
-                  live. The commitment to eco-friendly living”</p>
-                <div class="testi-card-quote"><img src="assets/img/icon/testi-quote.svg" alt="img"></div>
-              </div>
-            </div>
-            <div class="swiper-slide">
-              <div class="testi-card">
-                <div class="testi-card_wrapper">
-                  <div class="testi-card_profile">
-                    <div class="testi-card_avater"><img src="assets/img/testimonial/testi_1_2.jpg" alt="testimonial">
-                    </div>
-                    <div class="media-body">
-                      <h3 class="box-title">Andrew Simon</h3><span class="testi-card_desig">Traveller</span>
-                    </div>
-                  </div>
-                  <div class="testi-card_review"><i class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i> <i
-                      class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i>
-                  </div>
-                </div>
-                <p class="testi-card_text">A sophisticated rainwater harvesting system collects and filters rainwater
-                  for irrigation and non-potable uses, reducing reliance on municipal water sources. Greywater systems
-                </p>
-                <div class="testi-card-quote"><img src="assets/img/icon/testi-quote.svg" alt="img"></div>
-              </div>
-            </div>
-            <div class="swiper-slide">
-              <div class="testi-card">
-                <div class="testi-card_wrapper">
-                  <div class="testi-card_profile">
-                    <div class="testi-card_avater"><img src="assets/img/testimonial/testi_1_1.jpg" alt="testimonial">
-                    </div>
-                    <div class="media-body">
-                      <h3 class="box-title">Alex Jordan</h3><span class="testi-card_desig">Traveller</span>
-                    </div>
-                  </div>
-                  <div class="testi-card_review"><i class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i> <i
-                      class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i>
-                  </div>
-                </div>
-                <p class="testi-card_text">Throughout the interior, eco-friendly materials like reclaimed wood, bamboo
-                  flooring, and recycled glass countertops create a luxurious yet sustainable ambiance.</p>
+                <p class="testi-card_text">Their customer care is excellent!.</p>
                 <div class="testi-card-quote"><img src="assets/img/icon/testi-quote.svg" alt="img"></div>
               </div>
             </div>
