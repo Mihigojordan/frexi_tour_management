18dab9243527bdcfdbd8f4470a205a95e51c6b96
12th-ai <mihigofololive@gmail.com>
2024-11-22 10:26:15 +0200

    fix search bar

--- Components/navbar.php
+++ Components/navbar.php
@@ -61,72 +61,90 @@
 </head>
 <body>
 <div class="sidemenu-wrapper sidemenu-info">
-    <div class="sidemenu-content"><button class="closeButton sideMenuCls"><i class="far fa-times"></i></button>
-      <div class="widget">
-        <div class="th-widget-about">
-          <div class="about-logo"><a href="home-travel.html"><img src="assets/img/logo2.svg" alt="Tourm"></a></div>
-          <p class="about-text">Rapidiously myocardinate cross-platform intellectual capital model. Appropriately create
-            interactive infrastructures</p>
-          <div class="th-social"><a href="https://www.facebook.com/"><i class="fab fa-facebook-f"></i></a> <a
-              href="https://www.twitter.com/"><i class="fab fa-twitter"></i></a> <a href="https://www.linkedin.com/"><i
-                class="fab fa-linkedin-in"></i></a> <a href="https://www.whatsapp.com/"><i
-                class="fab fa-whatsapp"></i></a></div>
+  <div class="sidemenu-content">
+    <button class="closeButton sideMenuCls"><i class="far fa-times"></i></button>
+    <div class="widget">
+      <div class="th-widget-about">
+        <div class="about-logo">
+          <a href="index.php"><img src="assets/img/logo2.svg" alt="Frexi - Explore Rwanda"></a>
+        </div>
+        <p class="about-text">
+          Discover the breathtaking beauty of Rwanda with Frexi Tours! From the majestic mountain gorillas of Volcanoes National Park to the serene shores of Lake Kivu, explore Rwanda's unique landscapes, rich culture, and vibrant cities. Let us guide you on an unforgettable journey.
+        </p>
+        <div class="th-social">
+          <a href="https://www.facebook.com/"><i class="fab fa-facebook-f"></i></a>
+          <a href="https://www.twitter.com/"><i class="fab fa-twitter"></i></a>
+          <a href="https://www.linkedin.com/"><i class="fab fa-linkedin-in"></i></a>
+          <a href="https://www.whatsapp.com/"><i class="fab fa-whatsapp"></i></a>
         </div>
       </div>
-      <div class="widget">
-        <h3 class="widget_title">Recent Posts</h3>
-        <div class="recent-post-wrap">
-          <div class="recent-post">
-            <div class="media-img"><a href="blog-details.html"><img src="assets/img/blog/recent-post-1-1.jpg"
-                  alt="Blog Image"></a></div>
-            <div class="media-body">
-              <div class="recent-post-meta"><a href="blog.html"><i class="far fa-calendar"></i>24 Jun , 2024</a></div>
-              <h4 class="post-title"><a class="text-inherit" href="blog-details.html">Where Vision Meets Concrete
-                  Reality</a></h4>
-            </div>
+    </div>
+    <div class="widget">
+      <h3 class="widget_title">Explore Rwanda</h3>
+      <div class="recent-post-wrap">
+        <div class="recent-post">
+          <div class="media-img">
+            <a href="blog-details.html"><img src="assets/img/blog/recent-post-rwanda.jpg" alt="Discover Rwanda"></a>
           </div>
-          <div class="recent-post">
-            <div class="media-img"><a href="blog-details.html"><img src="assets/img/blog/recent-post-1-2.jpg"
-                  alt="Blog Image"></a></div>
-            <div class="media-body">
-              <div class="recent-post-meta"><a href="blog.html"><i class="far fa-calendar"></i>22 Jun , 2024</a></div>
-              <h4 class="post-title"><a class="text-inherit" href="blog-details.html">Raising the Bar in
-                  Construction.</a></h4>
+          <div class="media-body">
+            <div class="recent-post-meta">
+              <a href="blog.html"><i class="far fa-calendar"></i>24 Jun , 2024</a>
             </div>
+            <h4 class="post-title">
+              <a class="text-inherit" href="blog-details.html">Top Attractions in Rwanda</a>
+            </h4>
+          </div>
+        </div>
+        <div class="recent-post">
+          <div class="media-img">
+            <a href="blog-details.html"><img src="assets/img/blog/recent-post-safari.jpg" alt="Rwanda Safari"></a>
+          </div>
+          <div class="media-body">
+            <div class="recent-post-meta">
+              <a href="blog.html"><i class="far fa-calendar"></i>22 Jun , 2024</a>
+            </div>
+            <h4 class="post-title">
+              <a class="text-inherit" href="blog-details.html">Why Rwanda is Africa's Hidden Gem</a>
+            </h4>
           </div>
         </div>
       </div>
-      <div class="widget">
-        <h3 class="widget_title">Get In Touch</h3>
-        <div class="th-widget-contact">
-          <div class="info-box_text">
-            <div class="icon"><img src="assets/img/icon/phone.svg" alt="img"></div>
-            <div class="details">
-              <p><a href="tel:+01234567890" class="info-box_link">+01 234 567 890</a></p>
-              <p><a href="tel:+09876543210" class="info-box_link">+09 876 543 210</a></p>
-            </div>
+    </div>
+    <div class="widget">
+      <h3 class="widget_title">Get In Touch</h3>
+      <div class="th-widget-contact">
+        <div class="info-box_text">
+          <div class="icon"><img src="assets/img/icon/phone.svg" alt="Phone"></div>
+          <div class="details">
+            <p><a href="tel:+250123456789" class="info-box_link">+250 123 456 789</a></p>
+            <p><a href="tel:+250987654321" class="info-box_link">+250 987 654 321</a></p>
           </div>
-          <div class="info-box_text">
-            <div class="icon"><img src="assets/img/icon/envelope.svg" alt="img"></div>
-            <div class="details">
-              <p><a href="mailto:mailinfo00@tourm.com" class="info-box_link">mailinfo00@tourm.com</a></p>
-              <p><a href="mailto:support24@tourm.com" class="info-box_link">support24@tourm.com</a></p>
-            </div>
+        </div>
+        <div class="info-box_text">
+          <div class="icon"><img src="assets/img/icon/envelope.svg" alt="Email"></div>
+          <div class="details">
+            <p><a href="mailto:info@frexitours.rw" class="info-box_link">info@frexitours.rw</a></p>
+            <p><a href="mailto:support@frexitours.rw" class="info-box_link">support@frexitours.rw</a></p>
           </div>
-          <div class="info-box_text">
-            <div class="icon"><img src="assets/img/icon/location-dot.svg" alt="img"></div>
-            <div class="details">
-              <p>789 Inner Lane, Holy park, California, USA</p>
-            </div>
+        </div>
+        <div class="info-box_text">
+          <div class="icon"><img src="assets/img/icon/location-dot.svg" alt="Location"></div>
+          <div class="details">
+            <p>Kigali, Rwanda - Your gateway to unforgettable adventures.</p>
           </div>
         </div>
       </div>
     </div>
   </div>
-  <div class="popup-search-box"><button class="searchClose"><i class="fal fa-times"></i></button>
-    <form action="#"><input type="text" placeholder="What are you looking for?"> <button type="submit"><i
-          class="fal fa-search"></i></button></form>
-  </div>
+</div>
+<div class="popup-search-box">
+  <button class="searchClose"><i class="fal fa-times"></i></button>
+  <form action="#">
+    <input type="text" placeholder="Search for Rwanda tours, gorilla trekking, or safaris">
+    <button type="submit"><i class="fal fa-search"></i></button>
+  </form>
+</div>
+
   <div class="th-menu-wrapper onepage-nav" >
     <div class="th-menu-area text-center"><button class="th-menu-toggle"><i class="fal fa-times"></i></button>
       <div class="mobile-logo"><a href="home-travel.html"><img src="assets/img/frexilogo.png" alt="Tourm"></a></div>
